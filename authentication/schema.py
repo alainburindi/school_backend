d21b7a764de19129123c5db31966a420a7fcb2d7
alain burindi <alainburindi62@gmail.com>
2019-11-10 13:12:22 +0200

    update link and create user

---
+++ authentication/schema.py
@@ -0,0 +1,36 @@
+from django.contrib.auth import get_user_model
+
+import graphene
+from graphene_django import DjangoObjectType
+
+
+class UserType(DjangoObjectType):
+    class Meta:
+        model = get_user_model()
+
+
+class CreateUser(graphene.Mutation):
+    user = graphene.Field(UserType)
+    message = graphene.String()
+
+    class Arguments:
+        username = graphene.String(required=True)
+        email = graphene.String(required=True)
+        password = graphene.String(required=True)
+
+    def mutate(self, info, **kwargs):
+        user = get_user_model()(
+            username=kwargs.get('username'),
+            email=kwargs.get('email')
+        )
+        user.set_password(kwargs.get('password'))
+        user.save()
+
+        return CreateUser(
+            user=user,
+            message="user created successfully"
+        )
+
+
+class Mutation(graphene.ObjectType):
+    create_user = CreateUser.Field()
