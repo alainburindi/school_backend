d5f2e0e2b809b4fa40f18deed8c98bf80c0df6a1
alainburindi <alainburindi62@gmail.com>
2020-01-09 18:31:04 +0200

    send an error message if the user is not logged in

--- school/apps/authentication/schema.py
+++ school/apps/authentication/schema.py
@@ -7,17 +7,3 @@ from .mutations.login import LoginUser
 class Mutation(graphene.ObjectType):
     create_user = CreateUser.Field()
     login = LoginUser.Field()
-
-# class Query(graphene.ObjectType):
-#     users = graphene.List(UserType)
-#     me = graphene.Field(UserType)
-
-#     def resolve_users(self, info):
-#         return User.objects.all()
-
-#     def resolve_me(self, info):
-#         user = info.context.user
-#         if user.is_anonymous:
-#             raise GraphQLError('not logged in!')
-
-#         return user
